8c4293faf1e1968d3b5511c36f10965b49e12a4c
RendyBagusHM <rendycalai@gmail.com>
2022-05-18 13:13:54 +0700

    Percobaan Ke 2

--- models/sales.py
+++ models/sales.py
@@ -5,24 +5,29 @@ class Sales_MO(models.Model):
     _inherit = 'sale.order'
 
     def mo_button(self):
-        mrp = []
+        # mrp = []
         for record in self.order_line:
             for bom_id in record.product_id.bom_ids:
                 bom = record.env['mrp.bom'].search([('id', '=', bom_id.id)]).read()
                 if bool(bom) == True:
-                    mrp.append(product_id.id)
+                    # mrp.append(product_id)
                     record.env['mrp.production'].create(
                         {'product_id': record.product_id.id, 
                         'product_uom_id': record.product_id.bom_ids.product_uom_id.id,
                         'bom_id': bom_id.id,  
                         'product_qty': record.product_uom_qty})
-
-        if len(mrp) == 1: 
-            bom = self.product_id.get_formviewaction()
-        else:
-            return{
-                    'name': "New",
+        return{
                     'type': 'ir.actions.act_window',
                     'res_model': 'mrp.production',
-                    'view_mode': 'tree',
+                    'view_mode': 'form',
                     }       
+
+        # if len(mrp) == 1: 
+        #     bom = self.product_id.get_formviewaction()
+        # else:
+        #     return{
+        #             'name': "New",
+        #             'type': 'ir.actions.act_window',
+        #             'res_model': 'mrp.production',
+        #             'view_mode': 'tree',
+        #             }       
